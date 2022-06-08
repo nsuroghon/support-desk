598f190375d64274dfd4c3852ff6360351a39101
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-06-08 16:00:09 -0400

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">My App</div>
-  );
+  <>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+        </Routes>
+      </div>
+    </Router>
+  
+  </>
+  )
 }
 
 export default App;
