4d3357ba06386944359c0265148b057609d9d970
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-06-12 10:53:40 -0400

    login UI, register UI, toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+// components
+import Header from './components/Header';
 // Pages
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -10,6 +13,7 @@ function App() {
   <>
     <Router>
       <div className="container">
+        <Header />
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/login' element={<Login />}/>
@@ -17,6 +21,7 @@ function App() {
         </Routes>
       </div>
     </Router>
+    <ToastContainer />
   
   </>
   )
