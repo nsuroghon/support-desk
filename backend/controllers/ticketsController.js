cbf816db17e92726b0d9e8446e5b2e00bafb355a
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-09-22 14:58:56 -0400

    get ticket by id route

--- backend/controllers/ticketsController.js
+++ backend/controllers/ticketsController.js
@@ -8,12 +8,70 @@ module.exports = {
     // @route GET /api/tickets
     // @access Private
     getTickets: asyncHandler( async(req,res) => {
-        res.status(200).json({message: 'getTickets'})
+        const user = await db.User.findById(req.user.id)
+
+        if(!user) {
+            res.status(401)
+            throw new Error('User not found')
+        }
+
+        const tickets = await db.Ticket.find({user: req.user.id})
+
+        res.status(200).json(tickets)
     }),
+
+    // @desc Get single ticket
+    // @route GET /api/tickets/:id
+    // @access Private
+    getTicket: asyncHandler( async(req,res) => {
+        const user = await db.User.findById(req.user.id)
+
+        if(!user) {
+            res.status(401)
+            throw new Error('User not found')
+        }
+
+        const ticket = await db.Ticket.findById(req.params.id)
+
+        if(!ticket){
+            res.status(404)
+            throw new Error('Ticket Not found')
+        }
+
+        if(ticket.user.toString() !== req.user.id) {
+            res.status(401)
+            throw new Error('Not Authorized')
+        }
+
+
+        res.status(200).json(ticket)
+    }),
+
     // @desc Create new ticket
     // @route POST /api/tickets
     // @access Private
     createTicket: asyncHandler( async(req,res) => {
-        res.status(200).json({message: 'createTicket'})
+        const {product, description} = req.body
+
+        if(!product || !description) {
+            res.status(400)
+            throw new Error('Please add a product and description')
+        }
+
+        const user = await db.User.findById(req.user.id)
+
+        if(!user) {
+            res.status(401)
+            throw new Error('User not found')
+        }
+
+        const ticket = await db.Ticket.create({
+            product,
+            description,
+            user: req.user.id,
+            status: 'new'
+        })
+
+        res.status(201).json(ticket)
     })
 }
--- backend/routes/api/tickets.js
+++ backend/routes/api/tickets.js
@@ -3,7 +3,10 @@ const ticketsController = require('../../controllers/ticketsController')
 const {protect} = require('../../middleware/authMiddleware')
 
 router.route('/')
-    .get(protect, ticketsController.getTickets)
+    .get(protect, ticketsController.getTickets, ticketsController.getTicket )
     .post(protect, ticketsController.createTicket)
+
+router.route('/:id')
+    .get(protect, ticketsController.getTicket)
 
 module.exports = router
