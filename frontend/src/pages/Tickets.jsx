b48d28a0dcdf40f52b7d9467bf78fb09e13c29c2
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-10-21 16:34:38 -0400

    view singular ticket state, ui display

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import {useSelector, useDispatch} from 'react-redux'
 import { getTickets, reset } from "../features/tickets/ticketSlice"
 import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
+import TicketItem from "../components/TicketItem"
 
 const Tickets = () => {
   const {tickets, isLoading, isSuccess} = useSelector((state) =>
@@ -27,7 +28,21 @@ const Tickets = () => {
   }
 
   return (
-    <h1>Tickets</h1>
+    <>
+        <BackButton url="/"/>
+        <h1>Tickets</h1> 
+        <div className="tickets">
+            <div className="ticket-headings">
+                <div>Date</div>
+                <div>Product</div>
+                <div>Status</div>
+                <div></div>
+            </div>
+            {tickets.map((ticket) => (
+                <TicketItem key={ticket._id} ticket={ticket}/>
+            ))}
+        </div>
+    </>
   )
 }
 
