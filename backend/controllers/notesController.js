82efa15bdf0618969d5d8e6e7ab80c5e659459a9
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-11-02 20:57:43 -0400

    add note to ticket functionality

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -26,5 +26,33 @@ module.exports = {
 
         res.status(200).json(notes)
     }),
+    
+    // @desc Create ticket note
+    // @route POST /api/tickets/:ticketId/notes
+    // @access Private
+    addNote: asyncHandler( async(req,res) => {
+        const user = await db.User.findById(req.user.id)
+
+        if(!user) {
+            res.status(401)
+            throw new Error('User not found')
+        }
+
+        const ticket = await db.Ticket.findById(req.params.ticketId)
+
+        if(ticket.user.toString() !== req.user.id) {
+            res.status(401)
+            throw new Error('User not authorized')
+        }
+
+        const note = await db.Note.create({
+            text: req.body.text,
+            isStaff: false,
+            ticket: req.params.ticketId,
+            user: req.user.id
+        })
+
+        res.status(200).json(note)
+    }),
 
 }
--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -4,5 +4,6 @@ const {protect} = require('../../middleware/authMiddleware')
 
 router.route('/')
     .get(protect, notesController.getNotes )
+    .post(protect, notesController.addNote)
 
 module.exports = router
