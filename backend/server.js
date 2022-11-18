ec34b3b5fd1dcd331f5ed0aa024b5edf95159895
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-11-18 09:43:21 -0500

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -23,19 +23,20 @@ const routes = require('./routes')
 // Routes
 app.use(routes)
 
-// serve front end
-if(process.env.NODE_ENV === 'production'){
-    // set build folder as static
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
-
+  
+    // FIX: below code fixes app crashing on refresh in deployment
     app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-      })
-} else {
-    app.get('/', (req,res) => {
-        res.status(200).json({message: 'Welcome to the support desk API'})
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
     })
-}
+  } else {
+    app.get('/', (_, res) => {
+      res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    })
+  }
 
 // middleware
 app.use(errorHandler)
