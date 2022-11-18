de7cd0870deb7bd27f92836b531f5ddcaf38a5a3
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-11-17 20:00:36 -0500

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -13,14 +14,26 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req,res) => {
-    res.status(200).json({message: 'Welcome to the support desk API'})
-})
+// app.get('/', (req,res) => {
+//     res.status(200).json({message: 'Welcome to the support desk API'})
+// })
 
 const routes = require('./routes')
 
 // Routes
 app.use(routes)
+
+// serve front end
+if(process.env.node_env === 'production'){
+    // set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html') )
+} else {
+    app.get('/', (req,res) => {
+        res.status(200).json({message: 'Welcome to the support desk API'})
+    })
+}
 
 // middleware
 app.use(errorHandler)
